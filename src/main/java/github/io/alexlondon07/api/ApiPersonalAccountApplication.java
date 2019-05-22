ff4be84961dc60b7ffe641e243b4d9f65e3dacad
alexander.londono <alexander.londono@grupokonecta.com>
2019-05-22 08:53:55 -0500

    Add @CrossOrigin

--- src/main/java/github/io/alexlondon07/api/ApiPersonalAccountApplication.java
+++ src/main/java/github/io/alexlondon07/api/ApiPersonalAccountApplication.java
@@ -2,10 +2,13 @@ package github.io.alexlondon07.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class ApiPersonalAccountApplication {
 
 	  @RequestMapping("/")
