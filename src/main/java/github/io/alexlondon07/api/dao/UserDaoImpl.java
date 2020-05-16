cb283d7ab19acf3b0149bc6faa2ed771fcaadf3b
Juan Camilo Noriega Hoyos <camiloair@MacBook-Air-de-Juan-3.local>
2020-05-16 16:07:09 -0500

    Implementacion Dao

--- src/main/java/github/io/alexlondon07/api/dao/UserDaoImpl.java
+++ src/main/java/github/io/alexlondon07/api/dao/UserDaoImpl.java
@@ -5,36 +5,40 @@ import java.util.List;
 import github.io.alexlondon07.api.models.Client;
 import github.io.alexlondon07.api.models.Users;
 
-public class UserDaoImpl implements UserDao {
+public class UserDaoImpl extends AbstractSession implements UserDao {
 
 	@Override
 	public void saveUser(Users user) {
-		// TODO Auto-generated method stub
-
+		getSession().persist(user);
 	}
 
 	@Override
 	public void updateUser(Users user) {
-		// TODO Auto-generated method stub
+		getSession().update(user);
 
 	}
 
 	@Override
 	public void deleteUser(String userName) {
-		// TODO Auto-generated method stub
+		if (userName != null && !userName.isEmpty()) {
+			//getSession().delete(client);
+		}
 
 	}
 
 	@Override
 	public Client findByUserNameAndPwd(String userName, String pwd) {
-		// TODO Auto-generated method stub
-		return null;
+		return (Client) getSession().createQuery("from USERS where user = :user and pwd = :pwd ")
+				.setParameter("cellphone", userName).
+				setParameter("pwd", pwd).uniqueResult();
+		
 	}
+	
+	
 
 	@Override
 	public List<Users> findAllUser() {
-		// TODO Auto-generated method stub
-		return null;
+		return getSession().createQuery("from USERS").list();
 	}
 
 	@Override
